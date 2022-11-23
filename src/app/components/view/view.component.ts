73a5de76b56f5fb6ee368a038005972198789901
Alejandro Damas <alejandrodamas3d@gmail.com>
2022-11-23 14:27:51 +0100

    info window y tal

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { ListaEESSPrecio } from 'src/app/interfaces/gasResponse.interface';
 import { MunicipioResponse } from 'src/app/interfaces/municipios.interface';
 import { ProvinciasResponse } from 'src/app/interfaces/provincias.interface';
@@ -9,15 +10,17 @@ import { ProvinciasResponse } from 'src/app/interfaces/provincias.interface';
   styleUrls: ['./view.component.css'],
 })
 export class ViewComponent implements OnInit {
+  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
+
   selectedComb: string = '';
   valuemin: number = 1;
   valuemax: number = 3;
   provSelected: ProvinciasResponse | undefined;
   munSelected: MunicipioResponse | undefined;
 
-  markers = []  as  any;
+  markers = [] as any;
   filteredGasList: ListaEESSPrecio[] = [];
-  
+  infoContent = '';
   //GoogleMaps
   center: google.maps.LatLngLiteral = {
     lat: 37.38283,
@@ -29,11 +32,15 @@ export class ViewComponent implements OnInit {
     draggable: false,
   };
 
-  
   constructor() {}
 
   ngOnInit(): void {
-
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
   }
 
   addMunSelected(value: MunicipioResponse | undefined) {
@@ -53,30 +60,41 @@ export class ViewComponent implements OnInit {
   }
   addFilteredGL(value: ListaEESSPrecio[]) {
     this.filteredGasList = value;
-    this.dropMarker()
+    this.dropMarker();
   }
 
   dropMarker() {
     this.markers = [];
-    for(var i =0; i<this.filteredGasList.length; i++){
+    for (var i = 0; i < this.filteredGasList.length; i++) {
       this.markers.push({
         position: {
           lat: parseFloat(this.filteredGasList[i]['Latitud'].replace(',', '.')),
-          lng: parseFloat(this.filteredGasList[i]['Longitud (WGS84)'].replace(',', '.'))
+          lng: parseFloat(
+            this.filteredGasList[i]['Longitud (WGS84)'].replace(',', '.')
+          ),
         },
         label: {
           color: 'white',
           text: this.filteredGasList[i]['Rótulo'],
         },
         title: this.filteredGasList[i]['Rótulo'],
-        info: 'Marker info ' + (this.markers.length + 1),
+        info:
+          '<h2>'+this.filteredGasList[i]['Rótulo'] + '</h2>' +
+          '<b>Dirección: </b>' +
+          this.filteredGasList[i]['Dirección']+ '<br>'+
+          '<b>Provincia: </b>' +
+          this.filteredGasList[i]['Provincia']+ '<br>'+
+          '<b>Horario: </b>' +
+          this.filteredGasList[i]['Horario'],
         options: {
           animation: google.maps.Animation.DROP,
         },
-      })
-      console.log(this.markers[i].position);
-      console.log(this.filteredGasList[i]['Latitud'].replace(',', '.'))
-      console.log(this.filteredGasList[i]['Longitud (WGS84)'].replace(',', '.'))
+      });
     }
   }
+
+  openInfoWindow(marker: MapMarker,content: string) {
+    this.infoContent = content;
+    if (this.infoWindow != undefined) this.infoWindow.open(marker);
+}
 }
